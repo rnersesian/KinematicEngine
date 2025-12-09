0299f20c4335b4bfc2bb6fa0cb5c482a8eccda30
rnersesian <nersesian.robert@gmail.com>
2025-12-09 10:53:19 +0100

    modify main file

--- engine.py
+++ engine.py
@@ -2,75 +2,9 @@ from pyray import *
 from game_object import *
 import math
 
-init_window(1200, 900, "Hello")
-
-
-# self.gameobjects: list[GameObject] = [
-#     KinematicNode(position=Vector2(200, 200)),
-#     KinematicNode(position=Vector2(400, 300)),
-#     KinematicNode(position=Vector2(350, 550)),
-# ]
-
-# self.gameobjects[0].is_selected = True
-# self.selected: GameObject = None
-
-# self.time = 0
-# self.delta = 1.0/ 60.0
-
-# select_radius = 5
-
-
-# def select_on_clic(_gameobjects: list[GameObject]) -> GameObject:
-#     temp_selected = None
-#     selection_distance = math.inf
-#     mouse_position = get_mouse_position()
-    
-#     for g in _gameobjects:
-#         distance_to_object = vector2_length(vector2_subtract(mouse_position, g.position))
-#         if distance_to_object < select_radius and distance_to_object < selection_distance:
-#             temp_selected = g
-#     self.selected = temp_selected
-#     self.selected.is_selected = True
-
-
-
-# while not window_should_close():
-#     begin_drawing()
-#     clear_background(DARKGRAY)
-
-#     self.delta = get_frame_time()
-#     self.time += self.delta
-    
-#     if is_key_pressed(KEY_Q): # type: ignore
-#         self.gameobjects[self.selected].is_selected = False
-#         self.selected = (self.selected - 1) % len(self.gameobjects)
-#         self.gameobjects[self.selected].is_selected = True
-#     elif is_key_pressed(KEY_E): # type: ignore
-#         self.gameobjects[self.selected].is_selected = False
-#         self.selected = (self.selected + 1) % len(self.gameobjects)
-#         self.gameobjects[self.selected].is_selected = True
-
-#     for gameobject in self.gameobjects:
-#         gameobject.Draw()
-    
-#     mouse_delta = get_mouse_delta()
-#     if is_mouse_button_released(0) and vector2_length(mouse_delta) < 0.1:
-#         select_on_clic()
-#     elif is_mouse_button_down(0):
-#         try:
-#             self.selected.Translate(get_mouse_delta())
-#         except:
-#             pass
-#     end_drawing()
-# close_window()
-
-
-
-
-
 class GameEngine:
     
-    def __init__(self):
+    def __init__(self, window_width=1200, window_height=900, window_name="KinematicEngine"):
         self.gameobjects: list[GameObject] = [
             KinematicNode(position=Vector2(200, 200)),
             KinematicNode(position=Vector2(400, 300)),
@@ -82,10 +16,16 @@ class GameEngine:
         self.delta: float = 1.0 / 60.0
         self.time: float = 0
         
+        self.window_width = window_width
+        self.window_height = window_height
+        self.window_name = window_name
+        
         background_color: Color = DARKGRAY
         
         
     def Run(self):
+        init_window(self.window_width, self.window_height, self.window_name)
+        
         while not window_should_close():
             begin_drawing()
             clear_background(DARKGRAY)
